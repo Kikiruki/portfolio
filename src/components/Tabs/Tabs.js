f5c15fabf1fb9b7fc4bb8bf1aa15a69fb9edf135
alexofwallstreet <alexofwallstreet@gmail.com>
2023-09-30 22:19:19 +0300

    Router refactoring

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./Tabs.css";
-import {BrowserRouter as Router, NavLink} from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 
 function Tabs() {
     const workSvg = (

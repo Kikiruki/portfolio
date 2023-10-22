9286607a48cdc8f3f19ddf749287bc1cdfded94b
alexofwallstreet <alexofwallstreet@gmail.com>
2023-10-22 22:36:44 +0300

    title + description

--- src/components/LanguageSwitcher/LanguageSwitcher.js
+++ src/components/LanguageSwitcher/LanguageSwitcher.js
@@ -1,8 +1,8 @@
-import React, {useEffect} from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 
 function LanguageSwitcher() {
-    const {  i18n } = useTranslation();
+    const { i18n } = useTranslation();
 
     useEffect(() => {
         // Retrieve the selected language from local storage on component mount
@@ -16,14 +16,20 @@ function LanguageSwitcher() {
         // Change the language and store it in local storage
         i18n.changeLanguage(language);
         localStorage.setItem('selectedLanguage', language);
+
+        // Update the title and description based on the selected language
+        document.title = t('meta-title');
+        document.querySelector('meta[name="description"]').setAttribute('content', t('meta-description'));
     };
+
+    const { t } = useTranslation(); // Get the translation function
 
     return (
         <div>
-            <button className={i18n.language === "en" ? "primary" : ""} onClick={() => changeLanguage('en')}>
+            <button className={i18n.language === 'en' ? 'primary' : ''} onClick={() => changeLanguage('en')}>
                 ENG
             </button>
-            <button className={i18n.language === "ru" ? "primary" : ""} onClick={() => changeLanguage('ru')}>
+            <button className={i18n.language === 'ru' ? 'primary' : ''} onClick={() => changeLanguage('ru')}>
                 РУС
             </button>
         </div>
